fa5c747b0c45341fd2a1dd80c17ba88f070cba28
Pastooh22 <48555920+Pastooh22@users.noreply.github.com>
2020-01-22 00:38:25 +0100

    usuniecie pustego folderu

---
+++ Mini photoshop/mini-ps-canvas.js
@@ -0,0 +1,111 @@
+document.addEventListener('DOMContentLoaded', appStart)
+
+let canvas
+let ctx
+function appStart () {
+  canvas = document.querySelector('#canvas')
+  document
+    .querySelector('#btnDarken')
+    .addEventListener('click', () => darkenFilter())
+  document
+    .querySelector('#btnContrast')
+    .addEventListener('click', () => contrastFilter())
+document
+    .querySelector('#btnLighten')
+    .addEventListener('click', () => lightenFilter())
+
+  ctx = canvas.getContext('2d');
+    resize();
+
+  const srcImage = new Image()
+  srcImage.src = 'asd.jpg'
+  srcImage.addEventListener('load', () => {
+    drawImage(srcImage, 0, 0)
+    drawRect(100, 100, 200, 200)
+    drawCircle(400, 400, 100)
+  })
+}
+
+function darkenFilter (amount = 30) {
+  const canvasData = ctx.getImageData(0, 0, 1920, 1080)
+  for (let i = 0; i < canvasData.data.length; i += 4) {
+    // R
+    canvasData.data[i] -= amount
+    // G
+    canvasData.data[i + 1] -= amount
+    // B
+    canvasData.data[i + 2] -= amount
+  }
+  ctx.putImageData(canvasData, 0, 0)
+  console.log(canvasData.data)
+}
+
+function lightenFilter (amount = 30) {
+  const canvasData = ctx.getImageData(0, 0, 1920, 1080)
+  for (let i = 0; i < canvasData.data.length; i += 4) {
+    // R
+    canvasData.data[i-1] -= amount
+    // G
+    canvasData.data[i - 1] -= amount
+    // B
+    canvasData.data[i - 2] -= amount
+  }
+  ctx.putImageData(canvasData, 0, 0)
+  console.log(canvasData.data)
+}
+
+function contrastFilter (amount = 7) {
+  const canvasData = ctx.getImageData(0, 0, 1920, 1080)
+  for (let i = 0; i < canvasData.data.length; i += 4) {
+    const r = canvasData.data[i]
+    const g = canvasData.data[i + 1]
+    const b = canvasData.data[i + 2]
+
+    const avg = (r + g + b) / 3
+    if (avg <= 127) {
+      amount = -amount
+    }
+    canvasData.data[i] += amount
+    canvasData.data[i + 1] += amount
+    canvasData.data[i + 2] += amount
+  }
+  ctx.putImageData(canvasData, 0, 0)
+  console.log(canvasData.data)
+}
+
+function drawImage (img, x, y) {
+  ctx.drawImage(img, x, y)
+}
+
+window.addEventListener("resize", resize);
+document.addEventListener("mousemove", draw);
+document.addEventListener("mousedown", setPosition);
+document.addEventListener("mouseenter", setPosition);
+var pos = { x: 0, y: 0 };
+
+// new position from mouse events
+function setPosition(e) {
+  pos.x = e.clientX;
+  pos.y = e.clientY;
+}
+function resize() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
+function draw(e) {
+  if (e.buttons !== 1) return; // if wcisniety lpm
+
+  var color = document.getElementById("hex").value;
+
+  ctx.beginPath(); // rozpoczecie 
+
+  ctx.lineWidth = 20; 
+  ctx.lineCap = "round";
+  ctx.strokeStyle = color; // hex kolor
+
+  ctx.moveTo(pos.x, pos.y); // z pozycji
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y); // na pozycje
+
+  ctx.stroke(); // rysuje
+}
